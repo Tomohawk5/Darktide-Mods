447dbe57199c823278f7856b73b5de0d99b9310f
Tomohawk5 <41629446+Tomohawk5@users.noreply.github.com>
2023-04-08 16:00:43 +0100

    Code cleanup

--- volleyfiretimer/scripts/utils.lua
+++ volleyfiretimer/scripts/utils.lua
@@ -1,18 +1,5 @@
 local mod = get_mod("volleyfiretimer")
 
-mod._print_table = function(o)
-  if type(o) == 'table' then
-    local s = '{ '
-    for k, v in pairs(o) do
-      if type(k) ~= 'number' then k = '"' .. k .. '"' end
-      s = s .. '[' .. k .. '] = ' .. mod._print_table(v) .. ','
-    end
-    return s .. '} '
-  else
-    return tostring(o)
-  end
-end
-
 mod.lerp = function(a, b, t)
   return (1 - t) * a + t * b
 end
