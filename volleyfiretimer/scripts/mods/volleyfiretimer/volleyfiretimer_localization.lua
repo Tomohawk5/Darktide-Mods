64e3573864c5bfb4950a9b6ebdc86255fceeeae6
deluxghost <deluxghost@users.noreply.github.com>
2023-05-04 15:47:54 +0800

    add zh-cn localization

--- volleyfiretimer/scripts/mods/volleyfiretimer/volleyfiretimer_localization.lua
+++ volleyfiretimer/scripts/mods/volleyfiretimer/volleyfiretimer_localization.lua
@@ -2,197 +2,246 @@ return {
   mod_name = {
     en = "Volley Fire Timer",
     ru = "Таймер Залпового огня",
+    ["zh-cn"] = "齐射计时器",
   },
   mod_description = {
     en = "Shows a bar with the remaining duration of the veterans volley fire ability.",
     ru = "Volley Fire Timer - Показывает полосу и время оставшейся продолжительности действия способности Залповый огонь Ветерана, а также счётчик количества обновлений таймера способности.",
+    ["zh-cn"] = "显示老兵齐射技能剩余时间状态条。",
   },
   timer_bar_options = {
     en = "Bar",
     ru = "Полоса",
+    ["zh-cn"] = "状态条",
   },
   bar_enabled = {
     en = "Enabled",
     ru = "Включение",
+    ["zh-cn"] = "启用",
   },
   bar_enabled_description = {
     en = "A linear progress bar that shrinks with the remaining time.",
     ru = "Линейная полоса прогресса, которая уменьшается вместе с оставшимся временем.",
+    ["zh-cn"] = "随剩余时间而缩短的线性进度条。",
   },
   bar_border_enabled = {
     en = "Drop Shadow",
     ru = "Тень",
+    ["zh-cn"] = "阴影投影",
   },
   bar_border_enabled_description = {
     en = "Displays a small drop shadow below the progress bar.",
     ru = "Отображает небольшую тень под полосой прогресса.",
+    ["zh-cn"] = "在进度条下方略微显示阴影效果。",
   },
   bar_orientation = {
     en = "Orientation",
     ru = "Ориентация",
+    ["zh-cn"] = "朝向",
   },
   bar_orientation_description = {
     en = "Determines which direction the bar progress in.",
     ru = "Определяет, в каком направлении движется полоса.",
+    ["zh-cn"] = "设置状态条的朝向。",
   },
   bar_horizontal_option = {
     en = "Horizontal",
     ru = "Горизонтально",
+    ["zh-cn"] = "水平",
   },
   bar_vertical_option = {
     en = "Vertical",
     ru = "Вертикально",
+    ["zh-cn"] = "垂直",
   },
   bar_width = {
     en = "Width",
     ru = "Ширина",
+    ["zh-cn"] = "宽度",
   },
   bar_height = {
     en = "Height",
     ru = "Высота",
+    ["zh-cn"] = "高度",
   },
   text_options = {
     en = "Timer",
     ru = "Таймер",
+    ["zh-cn"] = "计时器",
   },
   text_enabled = {
     en = "Enabled",
     ru = "Включени",
+    ["zh-cn"] = "启用",
   },
   text_enabled_description = {
     en = "Numeric countdown showing remaining time in seconds.",
     ru = "Числовой обратный отсчёт, показывающий оставшееся время в секундах.",
+    ["zh-cn"] = "以秒数显示剩余时间倒计时。",
   },
   text_font_size = {
     en = "Size",
     ru = "Размер цифр",
+    ["zh-cn"] = "大小",
   },
   text_horizontal_offset = {
     en = "Horizontal offset",
     ru = "Горизонтальное смещение",
+    ["zh-cn"] = "水平偏移",
   },
   text_horizontal_offset_description = {
     en = "Horizontally offset the text timer from the progress bar.",
     ru = "Горизонтальное смещение текстового таймера от полосы прогресса.",
+    ["zh-cn"] = "计时器文本相对进度条的水平偏移量。",
   },
   text_vertical_offset = {
     en = "Vertical offset",
     ru = "Вертикальное смещение",
+    ["zh-cn"] = "垂直偏移",
   },
   text_vertical_offset_description = {
     en = "Vertically offset the text timer from the progress bar.",
     ru = "Вертикальное смещение текстового таймера от полосы прогресса.",
+    ["zh-cn"] = "计时器文本相对进度条的垂直偏移量。",
   },
   stack_options = {
     en = "Stack",
     ru = "Счётчик",
+    ["zh-cn"] = "层数",
   },
   stack_enabled = {
     en = "Enabled",
     ru = "Включение",
+    ["zh-cn"] = "启用",
   },
   stack_enabled_description = {
     en = "Displays the number of times you have refreshed the volley fire buff.",
     ru = "Показывает сколько раз вы обновляли бафф Залпового огня.",
+    ["zh-cn"] = "显示你已经刷新齐射增益的次数。",
   },
   stack_alert = {
     en = "Notification",
     ru = "Уведомление",
+    ["zh-cn"] = "通知提醒",
   },
   stack_alert_description = {
     en = "Displays a notification when volley fire ends of how many times it was refreshed and how long it lasted.",
     ru = "Отображает уведомление, после окончания действия Залпового огня, сколько раз он обновлялся и как долго длился.",
+    ["zh-cn"] = "在齐射结束时，显示技能刷新次数和总持续时间的通知消息。",
   },
   stack_font_size = {
     en = "Size",
     ru = "Размер цифр",
+    ["zh-cn"] = "大小",
   },
   stack_horizontal_offset = {
     en = "Horizontal offset",
     ru = "Горизонтальное смещение",
+    ["zh-cn"] = "水平偏移",
   },
   stack_horizontal_offset_description = {
     en = "Horizontally offset the stack count from the progress bar.",
     ru = "Горизонтальное смещение счётчика от полосы прогресса.",
+    ["zh-cn"] = "层数相对进度条的水平偏移量。",
   },
   stack_vertical_offset = {
     en = "Vertical offset",
     ru = "Вертикальное смещение",
+    ["zh-cn"] = "垂直偏移",
   },
   stack_vertical_offset_description = {
     en = "Vertically offset the stack count from the progress bar.",
     ru = "Вертикальное смещение счётчика от полосы прогресса.",
+    ["zh-cn"] = "层数相对进度条的垂直偏移量。",
   },
   bar_color = {
     en = "Bar Color",
     ru = "Цвет полосы",
+    ["zh-cn"] = "状态条颜色",
   },
   bar_color_r = {
     en = "Red",
     ru = "Красный",
+    ["zh-cn"] = "红色",
   },
   bar_color_g = {
     en = "Green",
     ru = "Зелёный",
+    ["zh-cn"] = "绿色",
   },
   bar_color_b = {
     en = "Blue",
     ru = "Синий",
+    ["zh-cn"] = "蓝色",
   },
   bar_opacity = {
     en = "Opacity",
     ru = "Прозрачность",
+    ["zh-cn"] = "不透明度",
   },
   text_color = {
     en = "Timer Color",
     ru = "Цвет таймера",
+    ["zh-cn"] = "计时器颜色",
   },
   text_color_r = {
     en = "Red",
     ru = "Красный",
+    ["zh-cn"] = "红色",
   },
   text_color_g = {
     en = "Green",
     ru = "Зелёный",
+    ["zh-cn"] = "绿色",
   },
   text_color_b = {
     en = "Blue",
     ru = "Синий",
+    ["zh-cn"] = "蓝色",
   },
   text_opacity = {
     en = "Opacity",
     ru = "Прозрачность",
+    ["zh-cn"] = "不透明度",
   },
   stack_color = {
     en = "Stack Color",
     ru = "Цвет счётчика",
+    ["zh-cn"] = "层数颜色",
   },
   stack_color_r = {
     en = "Red",
     ru = "Красный",
+    ["zh-cn"] = "红色",
   },
   stack_color_g = {
     en = "Green",
     ru = "Зелёный",
+    ["zh-cn"] = "绿色",
   },
   stack_color_b = {
     en = "Blue",
     ru = "Синий",
+    ["zh-cn"] = "蓝色",
   },
   stack_opacity = {
     en = "Opacity",
     ru = "Прозрачность",
+    ["zh-cn"] = "不透明度",
   },
   mod_options = {
     en = "Miscellaneous",
     ru = "Разное",
+    ["zh-cn"] = "杂项",
   },
   always_visible = {
     en = "Always visible",
     ru = "Всегда видно",
+    ["zh-cn"] = "总是显示",
   },
   always_visible_description = {
     en = "Show the hud elements at all times, not just during volley fire.",
     ru = "Показывать элементы интерфейса всегда, а не только во время Залпового огня.",
+    ["zh-cn"] = "即使未发动齐射技能，也总是显示 HUD 组件。",
   }
 }
