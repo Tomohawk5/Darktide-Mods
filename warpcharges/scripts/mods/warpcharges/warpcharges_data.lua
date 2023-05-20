774c1475683fc7255321b609e43026851cf6cdf6
Tomohawk5 <41629446+Tomohawk5@users.noreply.github.com>
2023-05-20 01:02:03 +0100

    Added procedural colour options to data and localization

--- warpcharges/scripts/mods/warpcharges/warpcharges_data.lua
+++ warpcharges/scripts/mods/warpcharges/warpcharges_data.lua
@@ -19,6 +19,52 @@ mod.orientation_options = table.enum(
 	"orientation_option_vertical"
 )
 
+local colors = {}
+
+for _, color_name in ipairs(Color.list) do
+	table.insert(colors, { text = color_name, value = color_name })
+end
+
+table.sort(colors, function(a, b)
+	return a.text < b.text
+end)
+
+local function get_colors()
+	return table.clone(colors)
+end
+
+local function color_archetype(archetype)
+	local widget = {
+		setting_id = "color_" .. archetype,
+		type = "checkbox",
+		default_value = true,
+		sub_widgets = {
+			{
+				setting_id = "color_" .. archetype .. "_full",
+				type = "dropdown",
+				default_value = "ui_" .. archetype,
+				options = get_colors() --color_dropdown()
+			},
+			{
+				setting_id = "color_" .. archetype .. "_empty",
+				type = "dropdown",
+				default_value = "ui_" .. archetype .. "_text",
+				options = get_colors() --color_dropdown()
+			}
+		}
+	}
+	return widget
+end
+
+local function color_options()
+	local archetypes = { "psyker", "veteran", "zealot", "ogryn" }
+	local sub_widgets = {}
+	for _, archetype in pairs(archetypes) do
+		table.insert(sub_widgets, color_archetype(archetype))
+	end
+	return sub_widgets
+end 
+
 return {
 	name = mod:localize("mod_name"),
 	description = mod:localize("mod_description"),
@@ -26,53 +72,89 @@ return {
 	options = {
 		widgets = {
 			{
-				setting_id = "show_gauge",
-				type = "checkbox",
-				default_value = true
-			},
-			{
-				setting_id = "gauge_orientation",
-				type = "dropdown",
-				default_value = mod.orientation_options["orientation_option_horizontal"],
-				options = {
-					{ text = "orientation_option_horizontal", value = mod.orientation_options["orientation_option_horizontal"]	},
-					{ text = "orientation_option_vertical",	  value = mod.orientation_options["orientation_option_vertical"]	},
+				setting_id = "gauge",
+				type = "group",
+				sub_widgets = {
+					{
+						setting_id = "show_gauge",
+						type = "checkbox",
+						default_value = true
+					},
+					{
+						setting_id = "gauge_orientation",
+						type = "dropdown",
+						default_value = mod.orientation_options["orientation_option_horizontal"],
+						options = {
+							{ text = "orientation_option_horizontal", value = mod.orientation_options["orientation_option_horizontal"]	},
+							{ text = "orientation_option_vertical",	  value = mod.orientation_options["orientation_option_vertical"]	},
+						}
+					},
+					{
+						setting_id = "gauge_text",
+						type = "dropdown",
+						default_value = mod.text_options["text_option_warpcharges"],
+						options = {
+							{ text = "none",					value = mod.text_options["none"]					},
+							{ text = "text_option_charges",		value = mod.text_options["text_option_charges"]		},
+							{ text = "text_option_souls",		value = mod.text_options["text_option_souls"]		},
+							{ text = "text_option_warp",		value = mod.text_options["text_option_warp"]		},
+							{ text = "text_option_warpcharges",	value = mod.text_options["text_option_warpcharges"]	},
+						}
+					},
+					{
+						setting_id = "gauge_value",
+						type = "dropdown",
+						default_value = mod.value_options["value_option_stacks"],
+						options = {
+							{ text = "none",						value = mod.value_options["none"]						},
+							{ text = "value_option_damage",			value = mod.value_options["value_option_damage"]		},
+							{ text = "value_option_stacks",			value = mod.value_options["value_option_stacks"]		},
+							{ text = "value_option_time_percent",	value = mod.value_options["value_option_time_percent"]	},
+							{ text = "value_option_time_seconds",	value = mod.value_options["value_option_time_seconds"]	},
+						}
+					},
+					{
+						setting_id = "gauge_value_text",
+						type = "checkbox",
+						default_value = true
+					}
 				}
-			},
-			{
-				setting_id = "gauge_text",
-				type = "dropdown",
-				default_value = mod.text_options["text_option_warpcharges"],
-				options = {
-					{ text = "none",					value = mod.text_options["none"]					},
-					{ text = "text_option_charges",		value = mod.text_options["text_option_charges"]		},
-					{ text = "text_option_souls",		value = mod.text_options["text_option_souls"]		},
-					{ text = "text_option_warp",		value = mod.text_options["text_option_warp"]		},
-					{ text = "text_option_warpcharges",	value = mod.text_options["text_option_warpcharges"]	},
-				}
-			},
-			{
-				setting_id = "gauge_value",
-				type = "dropdown",
-				default_value = mod.value_options["value_option_stacks"],
-				options = {
-					{ text = "none",						value = mod.value_options["none"]						},
-					{ text = "value_option_damage",			value = mod.value_options["value_option_damage"]		},
-					{ text = "value_option_stacks",			value = mod.value_options["value_option_stacks"]		},
-					{ text = "value_option_time_percent",	value = mod.value_options["value_option_time_percent"]	},
-					{ text = "value_option_time_seconds",	value = mod.value_options["value_option_time_seconds"]	},
-				}
-			},
-			{
-				setting_id = "gauge_value_text",
-				type = "checkbox",
-				default_value = true
 			},
 			{
 				setting_id = "psyker_only",
 				type = "checkbox",
-				default_value = true
+				default_value = false
+			},
+			{
+				setting_id = "color_options",
+				type = "group",
+				sub_widgets = color_options()
 			}
+			-- {
+			-- 	setting_id = "color_options",
+			-- 	type = "group",
+			-- 	sub_widgets = {
+			-- 		{
+			-- 			setting_id = "color_psyker",
+			-- 			type = "checkbox",
+			-- 			default_value = false,
+			-- 			sub_widgets = {
+			-- 				{
+			-- 					setting_id = "color_psyker_full",
+			-- 					type = "dropdown",
+			-- 					default_value = "red",
+			-- 					options = color_dropdown()
+			-- 				},
+			-- 				{
+			-- 					setting_id = "color_psyker_empty",
+			-- 					type = "dropdown",
+			-- 					default_value = "red",
+			-- 					options = color_dropdown()
+			-- 				}
+			-- 			}
+			-- 		}
+			-- 	}
+			-- }
 		}
 	}
 }
--- warpcharges/scripts/mods/warpcharges/warpcharges_localization.lua
+++ warpcharges/scripts/mods/warpcharges/warpcharges_localization.lua
@@ -1,4 +1,7 @@
-return {
+local mod = get_mod("warpcharges")
+local InputUtils = mod:original_require("scripts/managers/input/input_utils")
+
+local localizations = {
 	mod_name = {
 		en = "Warp Charges",
 	},
@@ -72,3 +75,23 @@ return {
 		en = "T:",
 	},
 }
+
+local function readable(text)
+	local readable_string = ""
+	local tokens = string.split(text, "_")
+	for i, token in ipairs(tokens) do
+		local first_letter = string.sub(token, 1, 1)
+		token = string.format("%s%s", string.upper(first_letter), string.sub(token, 2))
+		readable_string = string.trim(string.format("%s %s", readable_string, token))
+	end
+	return readable_string
+end
+
+local color_names = Color.list
+for i, color_name in ipairs(color_names) do
+	local color_values = Color[color_name](255, true)
+	local text = InputUtils.apply_color_to_input_text(readable(color_name), color_values)
+	localizations[color_name] = { en = text }
+end
+
+return localizations
